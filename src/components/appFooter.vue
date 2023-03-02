26c3a3ef24dd6561aa212fe6bc0c4eee73b841e7
Giorgio.Bocchio <giorgiobocchio2@gmail.com>
2023-03-02 17:07:40 +0100

    inser comic

--- src/components/appFooter.vue
+++ src/components/appFooter.vue
@@ -9,9 +9,68 @@
         <div class="card"><img src="../assets/buy-comics-shop-locator.png" alt="">COMIC SHOP LOCATOR</div>
         <div class="card"><img src="../assets/buy-dc-power-visa.svg" alt="">DC POWER VISA</div>
     </div>
+    <div class="nav-bar">
+        <div class="link-list">
+            <div class="link-box">
+                <div>DC COMICS</div>
+                <ul>
+                    <li>Characters</li>
+                    <li>Characters</li>
+                    <li>Characters</li>
+                    <li>Characters</li>
+                    <li>Characters</li>
+                    <li>Characters</li>
+                    <li>Characters</li>
+                </ul>
+                <div>DC COMICS</div>
+                <ul>
+                    <li>Characters</li>
+                    <li>Characters</li>
+                </ul>
+            </div>
+            <div class="link-box">
+                <div>DC COMICS</div>
+                <ul>
+                    <li>Characters</li>
+                    <li>Characters</li>
+                    <li>Characters</li>
+                    <li>Characters</li>
+                    <li>Characters</li>
+                    <li>Characters</li>
+                    <li>Characters</li>
+                    <li>Characters</li>
+                    <li>Characters</li>
+                    <li>Characters</li>
+                    <li>Characters</li>
+                </ul>
+            </div>
+            <div class="link-box">
+                <div>DC COMICS</div>
+                <ul>
+                    <li>Characters</li>
+                    <li>Characters</li>
+                    <li>Characters</li>
+                    <li>Characters</li>
+                    <li>Characters</li>
+                </ul>
+            </div>
+        </div>
+        <img src="../assets/dc-logo-bg.png" alt="">
+    </div>
+    <div class="social-list">
+        <div class="button"> SIGN-UP NOW</div>
+        <div class="social-link">
+            <div>FOLLOW US</div>
+            <img src="../assets/footer-facebook.png" alt="">
+            <img src="../assets/footer-twitter.png" alt="">
+            <img src="../assets/footer-youtube.png" alt="">
+            <img src="../assets/footer-pinterest.png" alt="">
+            <img src="../assets/footer-periscope.png" alt="">
+        </div>
+    </div>
 </template>
 
-<style lang="scss">
+<style lang="scss" scoped>
     .shop-list{
         background-color: #0282F9;
         padding: 10px 220px;
@@ -28,6 +87,59 @@
                 margin-right: 15px;
                 margin-left: 15px;
             }
+        }
+    }
+    .nav-bar{
+        background-image: url(../assets/footer-bg.jpg);
+        padding: 10px 220px;
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        height: 300px;
+        overflow: hidden;
+        img{
+            height: 130%;
+        }
+        .link-list{
+            display: flex;
+            justify-content: space-between;
+            align-items: baseline;
+            div{
+
+                div{
+                    color: white;
+                    padding: 10px 10px 10px 0;
+                }
+                ul{
+                    padding: 0;
+                    margin: 0;
+                    li{
+                        list-style: none;
+                        color: #8b8d8b;
+                    }
+                }
+            }             
+        }
+ 
+    }
+    .social-list{
+        background-color: #303030;
+        padding: 10px 220px;
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        .button{
+            color: white;
+            background-color: #303030;
+            border: 3px solid #0876DC;
+            padding: 5px 10px;
+        }
+        .social-link{
+            display: flex;
+            justify-content: space-between;
+            align-items: center;
+            color: #0876DC;
+            width: 50%;
         }
     }
 
--- src/components/appMain.vue
+++ src/components/appMain.vue
@@ -1,16 +1,113 @@
 <script>
+    export default{
+        data() {
+            return {
+                comics: [
+                    {
+                        "thumb": "https://th.bing.com/th/id/OIP.5zQdYxrcDsh0Np37SE8uqgHaLY?pid=ImgDet&rs=1",
+                        "price": "$19.99",
+                        "series": "Action Comics",
+                        "type": "comic book"
+                    },
+                    {
+                        "thumb": "https://th.bing.com/th/id/OIP.5zQdYxrcDsh0Np37SE8uqgHaLY?pid=ImgDet&rs=1",
+                        "price": "$3.99",
+                        "series": "American Vampire 1976",
+                        "type": "comic book"
+                    },
+                    {
+                        "thumb": "https://th.bing.com/th/id/OIP.5zQdYxrcDsh0Np37SE8uqgHaLY?pid=ImgDet&rs=1",
+                        "price": "$16.99",
+                        "series": "Aquaman",
+                        "type": "graphic novel"
+                    },
+                    {
+                        "thumb": "https://th.bing.com/th/id/OIP.5zQdYxrcDsh0Np37SE8uqgHaLY?pid=ImgDet&rs=1",
+                        "price": "$2.99",
+                        "series": "Batgirl",
+                        "type": "comic book"
+                    },
+                    {
+                        "thumb": "https://th.bing.com/th/id/OIP.5zQdYxrcDsh0Np37SE8uqgHaLY?pid=ImgDet&rs=1",
+                        "price": "$3.99",
+                        "series": "Batman",
+                        "type": "comic book"
+                    },
+                    {
+                        "thumb": "https://th.bing.com/th/id/OIP.5zQdYxrcDsh0Np37SE8uqgHaLY?pid=ImgDet&rs=1",
+                        "price": "$2.99",
+                        "series": "Batman Beyond",
+                        "type": "comic book"
+                    },
+                    {
+                        "thumb": "https://th.bing.com/th/id/OIP.5zQdYxrcDsh0Np37SE8uqgHaLY?pid=ImgDet&rs=1",
+                        "price": "$3.99",
+                        "series": "Batman/Superman",
+                        "type": "comic book"
+                    },
+                    {
+                        "thumb": "https://th.bing.com/th/id/OIP.5zQdYxrcDsh0Np37SE8uqgHaLY?pid=ImgDet&rs=1",
+                        "price": "$4.99",
+                        "series": "Batman/Superman Annual",
+                        "type": "comic book"
+                    },
+                    {
+                        "thumb": "https://th.bing.com/th/id/OIP.5zQdYxrcDsh0Np37SE8uqgHaLY?pid=ImgDet&rs=1",
+                        "price": "$5.99",
+                        "series": "Batman: The Joker War Zone",
+                        "type": "comic book"
+                    },
+                    {
+                        "thumb": "https://th.bing.com/th/id/OIP.5zQdYxrcDsh0Np37SE8uqgHaLY?pid=ImgDet&rs=1",
+                        "price": "$6.99",
+                        "series": "Batman: Three Jokers",
+                        "type": "comic book"
+                    },
+                    {
+                        "thumb": "https://th.bing.com/th/id/OIP.5zQdYxrcDsh0Np37SE8uqgHaLY?pid=ImgDet&rs=1",
+                        "price": "$4.99",
+                        "series": "Batman: White Knight Presents: Harley Quinn",
+                        "type": "comic book"
+                    },
+                    {
+                        "thumb": "https://th.bing.com/th/id/OIP.5zQdYxrcDsh0Np37SE8uqgHaLY?pid=ImgDet&rs=1",
+                        "price": "$16.99",
+                        "series": "Catwoman",
+                        "type": "graphic novel"
+                    }
+                ]
+            }
+        }    
+    }
 </script>
 
 <template>
-    <div class="main">
-        content goes here
+    <div id="app" class="main">
+        <div v-for="(comic, index) in comics">
+            <img v-bind:src="comic.thumb" alt="">
+            <div>{{ comic.series }}</div>
+        </div>
     </div>
 </template>
 
-<style>
+<style lang="scss" scoped>
     .main{
         background-color: #1C1C1C;
         padding: 50px 200px;
         color: white;
+        display: flex;
+        flex-wrap: wrap;
+        justify-content: flex start;
+        div {
+            height: 200px;
+            width: 20%;
+            img{
+                width: 80%;
+                height: 80%;
+            }
+            div{
+                width: 100%;
+            }
+        }
     }
 </style>
